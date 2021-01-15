9ce4a584296950be1d88f6afb515717091b18110
FAYZA DAOUDI <foufadaoudi@gmail.com"git config --global user.email foufadaoudi@gmail.comgit config --global user.name 'Victorious1989'git config --global user.email 'foufadaoudi@gmail.com>
2021-01-15 07:46:46 -0500

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import useApplicationData from './hooks/useApplicationData';
-import './App.css';
 
-function App() {
-  
-  const {state, setState}  = useApplicationData();
+import React, { useState } from 'react'
+import useApplicationData from './hooks/useApplicationData'
+import LoginForm from './components/LoginForm'
+import RegisterForm from './components/RegisterForm'
 
-  
-const userList = state.users.map(user => <li>{user.name} {user.email}</li>)
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import Button from 'react-bootstrap/Button'
+
+export default function App () {
+  const adminUser = {
+    email: 'admin@admin.com',
+    password: 'admin'
+  }
+
+  const [user, setUser] = useState({ email: '' })
+  const [error, setError] = useState('')
+
+  const Register = details => {
+    console.log(details)
+    if (details.email === adminUser.email && details.password == adminUser.password) {
+      console.log('logged in')
+      setUser({
+        email: details.name
+      })
+    } else {
+      console.log('Not match')
+      setError('Error')
+    }
+  }
+
+
+  const Login = details => {
+    console.log(details)
+    if (details.email === adminUser.email && details.password == adminUser.password) {
+      console.log('logged in')
+      setUser({
+        email: details.name
+      })
+    } else {
+      console.log('Not match')
+      setError('Error')
+    }
+  }
+
+  const Logout = details => {
+    console.log('LOGOUT')
+    setUser({
+      email: ''
+    })
+  }
 
   return (
-    <>
-    <h1>Users</h1>
+      <div className="App">
+        {(user.email !== '')
+          ? (
+            <div className ="Welcome">
+                <h2 className="text-center"> Welcome  </h2>
+                <Button className="btn-lg btn-dark btn-block" onClick={Logout}> Logout  </Button>
+               
+            </div>
+            )
+          : (
+     //   <LoginForm Login={Login} error={error}/>
+          <RegisterForm Register={Register} error={error}/>
 
-    <ul>
-      {userList}
-    </ul>
 
-    </>
-    );
+            )}
+
+    </div>
+
+  )
 }
-
-export default App;
