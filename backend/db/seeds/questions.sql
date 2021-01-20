05e7f210dd750eb452be6b37ea46977e329b1250
Areej Ba Salamah <areej.ombas@gmail.com>
2021-01-20 14:00:43 -0500

    edited the schema of the table and seeds

--- backend/db/seeds/questions.sql
+++ backend/db/seeds/questions.sql
@@ -1,56 +1,10 @@
-/* const questions = [
 
-
-  {
-    id: 'q1',
-    question: "Have you stayed sober since completing treatment?",
-    type: 'radio',
-    answers:["yes","no"]
-  },
-  {
-    id: 'q2',
-    question: "Have you found adequate housing?",
-    type: 'radio',
-    answers:["yes","no"]
-  },
-  {
-    id: 'q3',
-    question: "Are you currently employed?",
-    type: 'radio',
-    answers:["yes","no"]
-  },
-  {
-    id: 'q4',
-    question: "Have your family dynamics improved since coming home from treatment? ",
-    type: 'radio',
-    answers:["yes","no"]
-  },
-  {
-    id: 'q5',
-    question: "Have you found adequate housing?",
-    type: 'radio',
-    answers:["yes","no"]
-
-  },
-  {
-    id: 'q6',
-    question: "How likely are you to refer a friend or loved one to Cedar House for treatment?",
-    type: 'range',
-    answers:["1","2","3","4","5"]
-
-  },
-  {
-    id: 'q7',
-    question: "Do you have any additional comments to share with us?",
-    type: 'text',
-    
-  },
-  {
-    id:'q8',
-    question:"Are you currently on track or do you need assistance?",
-    type:'radio',
-    answers:['Yes I am on track', 'No I need assistance'],
-  
- }
-]
- */
+  INSERT INTO questions (survey_id,question_text, type) VALUES
+    (1,'Have you stayed sober since completing treatment?', 'radio'),
+    (1,'Have you found adequate housing?', 'radio'),
+    (1,'Are you currently employed?', 'radio'),
+    (1,'Have your family dynamics improved since coming home from treatment?', 'radio'),
+    (1,'How likely are you to refer a friend or loved one to Cedar House for treatment?', 'range'),
+    (1,'Do you have any additional comments to share with us?', 'text'),
+    (1,'Are you currently on track or do you need assistance?', 'radio')
+  ;
