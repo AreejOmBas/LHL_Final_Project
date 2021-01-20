47ae2c53bd1708521ac0fc90ce8c2a25f8a8bdca
Areej Ba Salamah <areej.ombas@gmail.com>
2021-01-20 13:57:33 -0500

    edited schema and seeded tables

--- backend/db/schema/create.sql
+++ backend/db/schema/create.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS clients CASCADE;
 DROP TABLE IF EXISTS surveys CASCADE;
 DROP TABLE IF EXISTS responses CASCADE;
-DROP TABLE IF EXISTS survey_questions CASCADE;
+DROP TABLE IF EXISTS sent_surveys CASCADE;
 DROP TABLE IF EXISTS questions CASCADE;
 
 
@@ -10,40 +10,42 @@ CREATE TABLE clients(
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  phone_num VARCHAR (10) NOT NULL,
+  phone_num VARCHAR (15) NOT NULL,
   password VARCHAR(255) NOT NULL,
   treatment_start_date  DATE  NOT NULL,
   treatment_end_date  DATE  NOT NULL,
   signup_date DATE NOT NULL,
-  next_survey_date DATE NOT NULL
+  next_survey_date DATE 
 );
 
 
 CREATE TABLE surveys(
   id SERIAL PRIMARY KEY NOT NULL,
-  survey_interval INTEGER NOT NULL,
-  date DATE NOT NULL
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE responses(
-   id SERIAL PRIMARY KEY NOT NULL,
-   survey_id INTEGER REFERENCES survey (id) ON DELETE CASCADE,
-   question_id INTEGER REFERENCES question (id) ON DELETE CASCADE,
-   client_id INTEGER REFERENCES client (id) ON DELETE CASCADE,
-   client_response VARCHAR(255),
-   Date VARCHAR (10) 
+  id SERIAL PRIMARY KEY NOT NULL,
+  sent_survey_id INTEGER REFERENCES sent_surveys(id) ON DELETE CASCADE,
+  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
+  client_response VARCHAR(255),
+  date DATE
 );
 
-CREATE TABLE survey_questions(
+CREATE TABLE sent_surveys(
   id SERIAL PRIMARY KEY NOT NULL,
-  survey_id INTEGER REFRENCES survey(id) ON DELETE CASCADE,
-  question_id INTEGER REFRENCES questions(id) ON DELETE CASCADE,
+  survey_id INTEGER REFERENCES surveys(id) ON DELETE CASCADE,
+  client_id INTEGER REFERENCES clients(id) ON DELETE CASCADE,
+  date DATE,
+  interval INTEGER
 );
 
 
 CREATE TABLE questions(
-   id SERIAL PRIMARY KEY NOT NULL,
-    question_text VARCHAR(255) NOT NULL,
-     type  VARCHAR(25) NOT NULL,
-     answer_id INTEGER REFRENCES possible_answers(id) ON DELETE CASCADE
+  id SERIAL PRIMARY KEY NOT NULL,
+  survey_id INTEGER REFERENCES surveys(id) ON DELETE CASCADE,
+  question_text VARCHAR(255) NOT NULL,
+  type  VARCHAR(25) NOT NULL
+    
 );
---
+++ backend/db/seeds/surveys.sql
@@ -0,0 +1,3 @@
+
+
+INSERT INTO surveys VALUES (1,'Cedar House Survey','Follow up Survey');
