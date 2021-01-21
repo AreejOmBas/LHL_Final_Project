228ef1b432eafe681f10631b1185af195870c618
Areej Ba Salamah <areej.ombas@gmail.com>
2021-01-21 18:21:42 -0500

    added valeus

--- backend/db/schema/create.sql
+++ backend/db/schema/create.sql
@@ -9,7 +9,7 @@ CREATE TABLE clients(
   id SERIAL PRIMARY KEY NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
   phone_num VARCHAR (15) NOT NULL,
   password VARCHAR(255) NOT NULL,
   treatment_start_date  DATE NOT NULL,
