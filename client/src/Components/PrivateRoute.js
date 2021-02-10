9a38d5618025907335e2d63fc8a35cd2a39a56d1
Areej Ba Salamah <areej.ombas@gmail.com>
2021-02-10 12:25:47 -0500

    clean up

--- client/src/Components/PrivateRoute.js
+++ client/src/Components/PrivateRoute.js
@@ -1,15 +1,16 @@
-import  React from  "react";
-import { Route, Redirect } from  "react-router-dom";
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
+/* Component to make survey route private not accessable unless user is logged in */
 
-const PrivateRoute =({component: Component, token, ...rest}) => {
+const PrivateRoute = ({ component: Component, token, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={(props) => token 
+      render={(props) => token
         ? <Component {...props} />
-        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+        : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
     />
   )
 }
-export  default  PrivateRoute;
+export default PrivateRoute;
--- client/src/Components/RegisterForm.js
+++ client/src/Components/RegisterForm.js
@@ -1,26 +1,29 @@
 import React, { useState } from "react";
 
 import { Form, Button } from "react-bootstrap";
-import "./Forms.css";
-import { Link } from 'react-router-dom';
+
 import MessageDialog from './MessageDialog'
 import DatePicker from 'react-datepicker';
 import axios from 'axios';
-import { useHistory,Redirect} from 'react-router-dom';
+
+import { BrowserRouter as Router, Route, Link, Switch, Redirect, useHistory } from 'react-router-dom';
+
+import "./Forms.css";
+
+/* Component for the registartion page */
 
 export default function RegistrationForm(props) {
 
   const [registerInformation, setregisterInformation] = useState
     ({ firstName: '', lastName: '', password: '', reEnterPassword: '', treatmentStartDate: '', treatmentEndDate: '', email: '', phoneNum: '' });
-  const { Register, error } = props;
   const [errorMsg, SetErrorMsg] = useState('');
   const [validated, setValidated] = useState(false);
- /// const [confirmationMsg, SetConfiramationMsg] = useState('');
+
   let history = useHistory();
 
-
-
+  //Handel Form submition
   const handleSubmit = (event) => {
+
     const form = event.currentTarget;
 
     if (form.checkValidity() === false) {
@@ -29,25 +32,19 @@ export default function RegistrationForm(props) {
 
 
     } else {
-      console.log('responses before post', registerInformation)
+
       axios.post(`/register/`, { ...registerInformation })
         .then((response) => {
           const confirmationMsg = response.data.message;
-    
-          console.log(confirmationMsg);
-       
-         history.push({
-          pathname: '/confirmation', 
-          state: {register: confirmationMsg} 
-        })
+          history.push({
+            pathname: '/confirmation',
+            state: { register: confirmationMsg }
+          })
         }
         )
         .catch(error => {
           if (error.response) {
-            console.log(error.response);
-
-
-            SetErrorMsg(error.response.data.msg)
+            SetErrorMsg(error.response.data.message)
           }
         })
     }
@@ -59,12 +56,11 @@ export default function RegistrationForm(props) {
   return (
     <center>
 
-
       <Form noValidate validated={validated} onSubmit={handleSubmit} className="login-form">
         <div className="form-header">
           <h1 className="text-center">Registration Form</h1>
 
-          <h3 className="text-center">Please Register to receive out monthly surveys</h3>
+          <h3 className="text-center">Please Register to receive our monthly surveys</h3>
           {errorMsg && <MessageDialog msg={errorMsg} />}
 
           <Form.Group className="form-input-container">
@@ -167,105 +163,4 @@ export default function RegistrationForm(props) {
       </Form>
     </center>
   )
-  // return (
-  //   <center>
-  //     <Form onSubmit={submitHandler} className="login-form">
-  //       <div className="form-inner">
-  //         <h3 className="text-center">Register here</h3>
-  //         {error !== '' ? <div className="error"> {error} </div> : ''}
-
-  //         <Form.Group>
-  //           <Form.Control
-  //             className="form-control form-control-lg"
-  //             type="text"
-  //             name="FirstName"
-  //             id="FirstName"
-  //             placeholder="First Name"
-  //             onChange={(e) =>
-  //               setregisterInformation({ ...registerInformation, FirstName: e.target.value })
-  //             }
-  //             value={registerInformation.FirstName}
-  //           />
-  //         </Form.Group>
-
-  //         <Form.Group>
-  //           <Form.Control
-  //             className="form-control form-control-lg"
-  //             type="text"
-  //             name="LastName"
-  //             id="LastName"
-  //             placeholder="Last Name"
-  //             onChange={(e) =>
-  //               setregisterInformation({ ...registerInformation, LastName: e.target.value })
-  //             }
-  //             value={registerInformation.LastName}
-  //           />
-  //         </Form.Group>
-
-  //         <Form.Group>
-  //           <Form.Control
-  //             className="form-control form-control-lg"
-  //             type="email"
-  //             name="email"
-  //             id="email"
-  //             placeholder="Email or Phone Number"
-  //             onChange={(e) =>
-  //               setregisterInformation({ ...registerInformation, email: e.target.value })
-  //             }
-  //             value={registerInformation.email}
-  //           />
-  //         </Form.Group>
-
-  //         <Form.Group>
-  //           <Form.Control
-  //             className="form-control form-control-lg"
-  //             type="password"
-  //             name="password"
-  //             id="password"
-  //             placeholder="Password"
-  //             onChange={(e) =>
-  //               setregisterInformation({ ...registerInformation, password: e.target.value })
-  //             }
-  //             value={registerInformation.password}
-  //           />
-  //         </Form.Group>
-
-  //         <Form.Group>
-  //           <Form.Control
-  //             className="form-control form-control-lg"
-  //             type="password"
-  //             name="Renterpassword"
-  //             id="Renterpassword"
-  //             placeholder="Renter Password"
-  //             onChange={(e) =>
-  //               setregisterInformation({ ...registerInformation, renterpassword: e.target.value })
-  //             }
-  //             value={registerInformation.password}
-  //           />
-  //         </Form.Group>
-
-  //         <Form.Group>
-  //           <Form.Control
-  //             className="form-control form-control-lg"
-  //             type="date"
-  //             name="date"
-  //             id="date"
-  //             placeholder="Renter Password"
-  //             onChange={(e) =>
-  //               setregisterInformation({ ...registerInformation, date: e.target.value })
-  //             }
-  //             value={registerInformation.password}
-  //           />
-  //         </Form.Group>
-
-  //         <Button className="btn-lg btn-dark btn-block btn-login" type="submit">
-  //           Sign up
-  //         </Button>
-  //         <div className="form-group col text-right">
-  //         <Link to="/login" className="link-text">You are a member?</Link>                
-  //         </div>
-  //       </div>
-  //     </Form>
-  //   </center>
-  // );
 }
